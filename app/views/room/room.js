b0f799e9a1db1984dd16758be6160724ddc02ae7
Sky <hyunwoo126@gmail.com>
2017-06-28 15:09:49 +0800

    basic websocket working

--- app/views/room/room.js
+++ app/views/room/room.js
@@ -2,6 +2,9 @@ var Observable = require("data/observable").fromObject;
 var ObservableArray = require("data/observable-array").ObservableArray;
 var dialogs = require("ui/dialogs");
 
+//https://github.com/NathanaelA/nativescript-websockets
+require('nativescript-websockets');
+
 var messagesList = new ObservableArray([]);
 
 var pageData = new Observable({
@@ -10,6 +13,7 @@ var pageData = new Observable({
     inputMsg: "",
 });
 
+var mySocket;
 var page;
 function loaded(args) {
     page = args.object;
@@ -25,10 +29,46 @@ function loaded(args) {
         }, 300);
     }
 
+    //websocket connection (nativescript-websockets)
+    mySocket = new WebSocket('ws://192.168.1.29:8080', [ /* "protocol","another protocol" */]);
+    mySocket.addEventListener('open', function (e){ 
+        console.log("Connection established!"); 
+    });
+    mySocket.addEventListener('message', function(e){
+         //console.log("We got a message: ", e.data); e.target.close();
+        var new_msg = JSON.parse(e.data);
+        new_msg.time = new_msg.timestamp;
+        messagesList.push(new_msg);
+        scrollToBottom(true);
+    });
+    mySocket.addEventListener('close', function(e){ 
+        console.log("The Socket was Closed:", e.code, e.reason); 
+    });
+    mySocket.addEventListener('error', function(e){
+        console.log("The socket had an error", e.error); 
+    });
+    
+
     //grab data from backend and add to pageData
     send('ini');
 }
 exports.loaded = loaded;
+
+function scrollToBottom(animate){
+    var view_list = page.getViewById('messagesList');
+    if(animate && page.android){
+        view_list.android.smoothScrollToPosition(view_list.items.length);
+    } else {
+        view_list.scrollToIndex(view_list.items.length);
+    }
+}
+
+function tapInput(){
+    setTimeout(function(){
+        scrollToBottom(true);
+    },100);
+}
+exports.tapInput = tapInput;
 
 function send(route){
     if(typeof route != 'string'){ var route = ''; }
@@ -56,6 +96,7 @@ function send(route){
     .then(function (r) {
         messagesList = new ObservableArray(r);
         pageData.set('messagesList', messagesList);
+        scrollToBottom(route == 'ini'?false:true);
     }).catch(function(done){
         console.log('error');
         console.log(done);
@@ -63,6 +104,7 @@ function send(route){
 
 
     if(route != 'ini'){
+        mySocket.send(JSON.stringify(body));
         pageData.set('inputMsg', '');
     }
 }
